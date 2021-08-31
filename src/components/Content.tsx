9ab288f706c953ecff013b8f1181265294e8ddf3
@costamatthew <mmateusdsilvacosta@outlook.com>
2021-08-30 21:01:02 -0300

    feat: creating Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,57 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import "./styles/content.scss";
+
+interface GenreResponseProps {
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface PropsContent {
+  selectedGenre: { title: string };
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+}
+
+export function Content({ selectedGenre, movies }: PropsContent) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
